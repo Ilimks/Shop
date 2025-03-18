7a503134de8bd395e3050ef0ce69cdb77bcf561d
windvenx <windvenx@mail.ru>
2025-03-18 14:12:29 +0600

    added mock data and part of single page

---
+++ src/app/product/[id]/page.tsx
@@ -0,0 +1,27 @@
+"use client";
+import React from "react";
+import mockClothingProducts from "@/app/mock";
+import { useParams } from "next/navigation";
+import MainProductImage from "./MainProductSection/MainProductSection";
+import { IProduct } from "../../types";
+
+const SingleProduct = () => {
+  const params = useParams();
+  const { id } = params;
+
+  const selectedProduct: IProduct | undefined = mockClothingProducts.find(
+    (product) => product.id === id
+  );
+
+  if (!selectedProduct) {
+    return <div>Продукт не найден</div>;
+  }
+
+  return (
+    <div>
+      <MainProductImage product={selectedProduct}/>
+    </div>
+  );
+};
+
+export default SingleProduct;
