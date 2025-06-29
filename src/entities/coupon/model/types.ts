9d73b0a1480fbe3a6c51367628eddd4987f4cbc8
xrisent <ormonov31261@gmail.com>
2025-06-29 21:02:31 +0600

    types

---
+++ src/entities/coupon/model/types.ts
@@ -0,0 +1,5 @@
+export interface Coupon{
+    coupon: string,
+    discount: number,
+    used: Boolean,
+}
--- src/entities/order/model/types.ts
+++ src/entities/order/model/types.ts
@@ -1,21 +1,23 @@
 import { Color } from "@/entities/color/model/types";
+import { Coupon } from "@/entities/coupon/model/types";
 import { Product } from "@/entities/product/model/types";
 import { Size } from "@/entities/size/model/types";
 import { User } from "@/entities/user/model/types";
 
 export interface OrderProduct {
-    product: Product;
-    color: Color;
-    size: Size;
-    quantity: number;
-  }
-  
-  export interface Order {
-    id: number;
-    user: User;
-    products: OrderProduct[];
-    createdAt: string;
-    finalPrice: number;
-    sold: boolean;
-    status?: 'pending' | 'completed' | 'cancelled';
-  }
+  product: Product;
+  color: Color;
+  size: Size;
+  quantity: number;
+}
+
+export interface Order {
+  id: number;
+  user: User;
+  products: OrderProduct[];
+  createdAt: string;
+  finalPrice: number;
+  sold: boolean;
+  coupon: Coupon;
+  status?: "pending" | "completed" | "cancelled";
+}
